32d61734b6cf9f7f8b4e8422d53dbe81ade75e1a
JakeOhcs <Jakeoh@JakeOhs-MacBook-Pro.local>
2016-09-27 12:48:13 -0400

    Add Three Amigo
    Fixed Error

--- MAD9111/Week4/Three Amigo/Three Amigo/mascotviewcontroller.swift
+++ MAD9111/Week4/Three Amigo/Three Amigo/mascotviewcontroller.swift
@@ -10,26 +10,47 @@ import UIKit
 
 class mascotViewController: UIViewController {
 
+    @IBOutlet weak var mascotImage: UIImageView!
+    @IBOutlet weak var mascotName: UILabel!
+    
+    var names: [String] = ["Androd","Duke","Tux"]
+    //var images:[UIImage] = [UIImage(named:"android.png")!,UIImage(named:"duck.png")!,UIImage(named:"tux.png")!]
+    
+    var images: [UIImage] = [
+        UIImage(named:"android.png")!,
+        UIImage(named:"duke.png")!,
+        UIImage(named:"tux.png")!
+    ]
+    var index:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        mascotName.text = names[index]
+        mascotImage.image = images[index]
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func handlePrevious(_ sender: AnyObject) {
+        
+        if(index <= 0){
+            index = names.count - 1
+        }else{
+            index -= 1
+        }
+        mascotName.text = names[index]
+        mascotImage.image = images[index]
+        
+        
     }
-    
+    @IBAction func handleNext(_ sender: AnyObject) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if(index >= names.count - 1){
+            index = 0
+        }
+        else{
+            index += 1
+        }
+        mascotName.text = names[index]
+        mascotImage.image = images[index]
     }
-    */
-
 }
